6528828f39235ec4864c1602afd608b95b5f0f8a
Büşra <busraakbulutt45@gmail.com>
2023-08-31 01:45:12 +0300

    homepage progress will be continue tomorrow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Container } from "../components/container";
+import { Hero, HeroTitle } from "@/components/hero";
+import { HeroSubtitle } from "../components/hero";
+import Image from "next/image";
+import image from "../public/hero.png";
 export default function Homepage() {
-  return <div>
-    <header></header>
-    <main></main>
-    <footer></footer>
-  </div>;
+	return (
+		<div>
+			<header>
+				<Container>header</Container>
+			</header>
+			<main>
+				<Container>
+					<Hero>
+						<HeroTitle>
+							Linear is a better way
+							<br />
+							to build products
+						</HeroTitle>
+						<HeroSubtitle>
+							Meet the new standard for modern software development.
+							<br />
+							Streamline issues, sprints, and product roadmaps.
+						</HeroSubtitle>
+						<Image src={image} alt="hero" width="1500" height="1500" />
+					</Hero>
+				</Container>
+			</main>
+			<footer>
+				<Container>footer</Container>
+			</footer>
+		</div>
+	);
 }
